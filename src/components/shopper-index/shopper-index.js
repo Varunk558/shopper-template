3aa4dbcedfab7e58829dd5091a417067399d6a07
Varun <varunkothapally18@gmail.com>
2026-04-23 16:50:25 -0500

    Added shopping related components like home, jewelery

--- src/components/shopper-index/shopper-index.js
+++ src/components/shopper-index/shopper-index.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ShopperHome } from "../shopper-home/shopper-home";
+import { ShopperJewelery } from "../shopper-jewlery/shopper-jewelery";
+import { ShopperCategory } from "../shopper-category/shopper-category";
+
 
 export function ShopperIndex() {
     return (
@@ -9,11 +13,11 @@ export function ShopperIndex() {
                         <h2>Shopper.</h2>
                     </div>
                     <nav className="d-flex gap-4">
-                        <div className="me-3">Home</div>
-                        <div className="me-3">Men's Fashion</div>
-                        <div className="me-3">Women's Fashion</div>
-                        <div className="me-3">Jewelry</div>
-                        <div className="me-3">Electronics</div>
+                        <div className="me-3"><Link to="/home" className="btn">Home</Link></div>
+                        <div className="me-3"><Link to="/mens-fashion" className="btn">Men's Fashion</Link></div>
+                        <div className="me-3"><Link to="/womens-fashion" className="btn">Women's Fashion</Link></div>
+                        <div className="me-3"><Link to="/jewelery" className="btn">Jewelery</Link></div>
+                        <div className="me-3"><Link to="/electronics" className="btn">Electronics</Link></div>
                     </nav>
                     <div>
                         <span className="bi bi-search me-3"></span>
@@ -23,7 +27,15 @@ export function ShopperIndex() {
                     </div>
                 </header>
                 <div className="mt-2 bg-dark text-white p-2 text-center">
-                    HAPPY HOLIDAY DEALS ON EVERYTHING! UP TO 50% OFF + FREE SHIPPING. SHOP NOW
+                    ⚡HAPPY HOLIDAY DEALS ON EVERYTHING! UP TO 50% OFF + FREE SHIPPING. SHOP NOW⚡
+                </div>
+                <div className="d-flex gap-4 flex-wrap justify-content-center mt-4">
+                    <Routes>
+                        <Route path="/" element ={<ShopperHome/>}/>
+                        <Route path="/home" element ={<ShopperHome/>}/>
+                        <Route path="/jewelery" element = {<ShopperJewelery/>}/>
+                        <Route path="/category" element = {<ShopperCategory/>}/>
+                    </Routes>
                 </div>
             </BrowserRouter>
         </div>
