cdb985ddcf6c5eca69f6f2df10c13de829e32f16
Varun <varunkothapally18@gmail.com>
2026-04-24 10:19:16 -0500

    Added code related to shopper home page

---
+++ src/components/shopper-home/shopper-home.js
@@ -0,0 +1,23 @@
+
+
+export function ShopperHome() {
+
+    return (
+        <div className="container-fluid">
+            <div className="d-flex gap-4 flex-wrap justify-content-between mt-4">
+                <div>
+                    <img src="mens.png" style={{ width: '200px', height: 'auto' }}/>
+                </div>
+                <div>
+                    <img src="womens.png" style={{ width: '200px', height: 'auto' }}/>
+                </div>
+                <div>
+                    <img src="jewlery.png" style={{ width: '200px', height: 'auto' }}/>
+                </div>
+                <div>
+                    <img src="electronics.png" style={{ width: '200px', height: 'auto' }}/>
+                </div>
+            </div>
+        </div>
+    )
+}
