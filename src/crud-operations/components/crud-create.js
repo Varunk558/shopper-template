74151f65c3f1ab695d036c8c98b6f0fee3116d2b
Varun <varunkothapally18@gmail.com>
2026-04-29 15:11:35 -0500

    Added code for validating productId and fetching products from the db

--- src/crud-operations/components/crud-create.js
+++ src/crud-operations/components/crud-create.js
@@ -1,9 +1,37 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 export function CrudCreate() {
     const navigate = useNavigate();
+    const [products, setProducts] = useState([]);
+    const [productIdError, setProductIdError] = useState("");
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://127.0.0.1:3500/products'
+        })
+            .then(response => {
+                const ids = response.data.map(product => product.ProductId);
+                setProducts(ids);
+            })
+            .catch(error => {
+                console.error("Error fetching products:", error);
+            });
+    }, []);
+
+
+    function VerifyId(event) {
+        for (var id of products) {
+            if (id == event.target.value) {
+                setProductIdError("ProductId already exists");
+                return;
+            }
+        }
+        setProductIdError("ProductId is available");
+    }
 
     return (
         <div className="container-fluid">
@@ -31,7 +59,8 @@ export function CrudCreate() {
                     <dl>
                         <dt>Product Id</dt>
                         <dd>
-                            <Field type="number" name="ProductId" />
+                            <Field type="number" name="ProductId" onKeyUp={VerifyId} />
+                            <dd className={productIdError.includes("already exists") ? "text-danger" : "text-success"}>{productIdError}</dd>
                         </dd>
                         <dt>Name</dt>
                         <dd>
