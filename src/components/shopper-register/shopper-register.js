29d15619f44c9152c5fb58d71b1461d60d54474c
Varun <varunkothapally18@gmail.com>
2026-04-29 15:09:52 -0500

    Added code for validating userId when registering the user

--- src/components/shopper-register/shopper-register.js
+++ src/components/shopper-register/shopper-register.js
@@ -2,11 +2,37 @@ import { useFormik, Formik, Form, ErrorMessage, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export function ShopperRegister() {
 
 
     const navigate = useNavigate();
+    const [users, setUsers] = useState([]);
+    const [userError, setUserError] = useState("");
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:3500/users'
+        })
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching users:", error);
+            });
+    }, []);
+
+    function VerifyUserId(event) {
+        for (var user of users) {
+            if (user.UserId == event.target.value) {
+                setUserError("UserId already exists");
+                return;
+            }
+        }
+        setUserError("UserId is available");
+    }
 
     return (
 
@@ -49,17 +75,18 @@ export function ShopperRegister() {
                 <Form>
                     <div>
                         <dt>UserId</dt>
-                        <dd><Field name="UserId" type="text"  /></dd>
+                        <dd><Field name="UserId" type="text" onKeyUp={VerifyUserId} /></dd>
                         <ErrorMessage name="UserId" component="div" className="text-danger" />
+                        <dd className={userError.includes("UserId already exists") ? "text-danger" : "text-success"}>{userError}</dd>
                     </div>
                     <div className="mb-3">
                         <dt>UserName</dt>
-                        <dd><Field name="UserName" type="text"  /></dd>
+                        <dd><Field name="UserName" type="text" /></dd>
                         <ErrorMessage name="UserName" component="div" className="text-danger" />
                     </div>
                     <div className="mb-3">
                         <dt>Email</dt>
-                        <dd><Field name="Email" type="email"  /></dd>
+                        <dd><Field name="Email" type="email" /></dd>
                         <ErrorMessage name="Email" component="div" className="text-danger" />
                     </div>
                     <div className="mb-3">
